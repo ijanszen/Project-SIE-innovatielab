0f779c7f9ce236098db29415134d0dc8b80889d1
PatrickDijkwel <Patrick.Dijkwel@student.hu.nl>
2019-04-03 11:32:15 +0200

    Test commit
    "Carrousel" toegevoegd

--- SIE-dashboard/src/App.vue
+++ SIE-dashboard/src/App.vue
@@ -2,16 +2,31 @@
   <div id="app">
     <NavBar />
      <router-view class="view"></router-view>
+    <carousel :per-page="1" :navigate-to="someLocalProperty" :mouse-drag="false" :autoplay="true" :autoplayTimeout="10000" :loop="true">
+      <slide>
+        <Home />
+      </slide>
+      <slide>
+        Slide 2 Content
+      </slide>
+    </carousel>
   </div>
 </template>
 
 <script>
 import NavBar from './components/layout/NavBar'
+import Home from './components/view/Home';
+
+import Vue from 'vue';
+import VueCarousel from 'vue-carousel';
+
+Vue.use(VueCarousel);
 
 export default {
   name: 'App',
   components: {
-    NavBar
+    NavBar,
+    Home
   }
 }
 </script>
