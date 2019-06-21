6b6febd6e643ba41e9053b7dad665ca3e4222e77
Idris Janszen <idris.janszen@student.hu.nl>
2019-06-21 17:16:19 +0200

    Made NanoLeafTable. it displays leaf#, color and if its on or on Doesnt use api data since the devices are not reachable from home Color has a build in color picker which is nice, thanks HTML5

---
+++ SIE-dashboard/src/components/common/NanoLeafTable.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-table striped :fields="fields" :items="items">
+    <template slot="id" slot-scope="data">{{ data.item.id}}</template>
+    <template slot="Kleurcode" slot-scope="data">
+      <b-form-input v-model="data.item.colorcode" id="type-color" type="color"></b-form-input>
+    </template>
+    <template slot="Ingeschakeld" slot-scope="data">
+      <b-form-checkbox v-model="data.item.on" name="check-button" switch></b-form-checkbox>
+    </template>
+  </b-table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fields: [{ key: "id", label: "Leaf #" }, "Kleurcode", "Ingeschakeld"],
+      items: [
+        { id: 1, colorcode: "#FF2000", on: true },
+        { id: 2, colorcode: "#208CC2", on: false },
+        { id: 3, colorcode: "#FF5733", on: true },
+        { id: 4, colorcode: "#E4B822", on: true }
+      ]
+    };
+  }
+};
+</script>
--- SIE-dashboard/src/components/view/NanoLeaf.vue
+++ SIE-dashboard/src/components/view/NanoLeaf.vue
@@ -1,28 +1,28 @@
 <template>
-  <b-container fluid>   
+  <b-container fluid>
     <b-row>
       <b-col>
-        <NanoLeafDescription />
-      </b-col>  
+        <NanoLeafDescription/>
+      </b-col>
+      <b-col>
+        <nano-leaf-table/>
+      </b-col>
     </b-row>
   </b-container>
 </template>
 
 <script>
-import NanoLeafDescription from '../common/DeviceDescriptions/NanoLeafDescription.vue'
-
+import NanoLeafDescription from "../common/DeviceDescriptions/NanoLeafDescription.vue";
+import NanoLeafTable from "../common//NanoLeafTable.vue";
 
 export default {
   components: {
     NanoLeafDescription,
+    NanoLeafTable
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.img {
-     max-width: 100%;
-     height: auto;
-  }
 </style>
